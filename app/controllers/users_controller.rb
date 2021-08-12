00561dc8c98bfe536b497c6e437bba1b0ca9b573
tak-ka3 <hi.ta9.23.333@gmail.com>
2021-08-12 13:42:47 +0900

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcom to the Sample App!"
       redirect_to @user #これはredirect_to user_url(@user)と等価
     else
