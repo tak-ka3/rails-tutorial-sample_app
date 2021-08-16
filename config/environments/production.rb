112e4beced62829937bb444720a6a0120afdcd7d
tak-ka3 <hi.ta9.23.333@gmail.com>
2021-08-16 14:17:30 +0900

    Change stmp_settings to smtp_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'https://rails-tutorial-mysampleapp.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.stmp_settings = {
+  ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
